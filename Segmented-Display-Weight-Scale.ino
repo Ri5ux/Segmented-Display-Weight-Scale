2783d27b29ecfce451c1e0226c25589d2bd46f50
Ri5ux <36822012+Ri5ux@users.noreply.github.com>
2019-04-10 00:25:07 -0500

    Added HX711 load cell functionality.

--- Segmented-Display-Weight-Scale.ino
+++ Segmented-Display-Weight-Scale.ino
@@ -28,18 +28,25 @@ static const int PIN_HX711_DAT = 17;
 long previousMillis = 0;
 
 int activeDisplay = 1;
-int displayBufferDriver1[DISPLAYS] = { 0, 0, 0 };
-int displayBufferDriver2[DISPLAYS] = { 0, 0, 0 };
-
-int count = 0;
-int weight = 0;
+int displayBufferDriver1[DISPLAYS] = { -1, -1, -1 };
+int displayBufferDriver2[DISPLAYS] = { -1, -1, -1 };
 
 boolean flip = false;
-boolean clearDisplays = false;
+
+HX711 scale;
+float calibration_factor = -5480;
+float scaleValue;
+long zero_factor;
 
 void setup()
 {
   Serial.begin(9600);
+  Serial.println("Multiplexed Segmented Display Scale");
+  Serial.println("Copyright (C) 2019 ASX Electronics");
+  Serial.println("Designed and built by Dustin Christensen");
+  Serial.println("");
+  Serial.println("Initializing...");
+  Serial.println("");
   
   pinMode(PINS_DISPLAY_DRIVER1[0], OUTPUT);
   pinMode(PINS_DISPLAY_DRIVER1[1], OUTPUT);
@@ -58,8 +65,18 @@ void setup()
   pinMode(PINS_DISPLAY_SELECT2[0], OUTPUT);
   pinMode(PINS_DISPLAY_SELECT2[1], OUTPUT);
   pinMode(PINS_DISPLAY_SELECT2[2], OUTPUT);
-
+  
   setUnitLbs();
+  startScale();
+}
+
+void startScale() {
+  scale.begin(PIN_HX711_DAT, PIN_HX711_CLK);
+  scale.set_scale();
+  scale.tare();
+  zero_factor = scale.read_average();
+  delay(500);
+  Serial.println("Scale initialized.");
 }
 
 void loop()
@@ -69,37 +86,55 @@ void loop()
   if(currentMillis - previousMillis > 100)
   {
     previousMillis = currentMillis;
-    count++;
-  
-    if (count > 9999)
-    {
-      count = 0;
-    }
-
-    weight = count;
+    
+    scale.set_scale(calibration_factor);
+    scaleValue = scale.get_units() + 0.07F;
   }
 
   updateWeight();
-  
-  delay(4);
 
+  clearDriver(PINS_DISPLAY_DRIVER1);
+  clearDriver(PINS_DISPLAY_DRIVER2);
   processDriver(PINS_DISPLAY_SELECT1, displayBufferDriver1, PINS_DISPLAY_DRIVER1);
   processDriver(PINS_DISPLAY_SELECT2, displayBufferDriver2, PINS_DISPLAY_DRIVER2);
-  
   activeDisplay++;
-  
+    
   if (activeDisplay > DISPLAYS)
   {
     activeDisplay = 1;
   }
+  
+  delay(4);
 }
 
 void updateWeight()
 {
-  int d1 = (weight / 1000) % 10;
-  int d2 = (weight / 100) % 10;
-  int d3 = (weight / 10) % 10;
-  int d4 = weight % 10;
+  int weight = round(scaleValue);
+  int d1 = 0;
+  int d2 = 0;
+  int d3 = 0;
+  int d4 = 0;
+  
+  if (weight > 0)
+  {
+    d1 = (weight / 1000) % 10;
+    d2 = (weight / 100) % 10;
+    d3 = (weight / 10) % 10;
+    d4 = weight % 10;
+  }
+
+  if (d1 == 0) d1 = -1;
+  if (d2 == 0) d2 = -1;
+  if (d3 == 0) d3 = -1;
+
+  if (weight < 0)
+  {
+    d1 = 0;
+    d2 = 0;
+    d3 = 0;
+    d4 = 0;
+  }
+  
   setWeightBuffer(d1, d2, d3, d4);
 }
 
@@ -115,14 +150,6 @@ void setUnitLbs()
 {
   displayBufferDriver2[1] = 1;
   displayBufferDriver2[2] = 6;
-}
-
-void clearDriver(int selectPins[])
-{
-  for (int displayId = 0; displayId < DISPLAYS; displayId++)
-  {
-    digitalWrite(selectPins[displayId], LOW);
-  }
 }
 
 void processDriver(int selectPins[], int displayBuffer[], int driverPins)
@@ -159,6 +186,17 @@ void writeIntToDisplayDriver(int num, int driverPins[])
   {
     int n = numMatrix[num][i];
     digitalWrite(driverPins[p], n);
+    p--;
+  }
+}
+
+void clearDriver(int driverPins[])
+{
+  int p = 3;
+  
+  for (int i = 0; i < 4; i++)
+  {
+    digitalWrite(driverPins[p], HIGH);
     p--;
   }
 }
